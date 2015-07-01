fcc22e4e2d950ffb7db9f7f1b9ece9eb44667b79
Patrick Keenan <pgk4526@gmail.com>
2015-07-01 12:30:58 -0400

    set up migration table and models.rb

--- models.rb
+++ models.rb
@@ -1,3 +1,18 @@
-Class User  < ActiveRecord::Base
+class User  < ActiveRecord::Base
+  has_one :profile
+  has_many :blogs
+end
+
+class Profile < ActiveRecord::Base
+  belongs_to :User
+end
+
+class Blog < ActiveRecord::Base
+  belongs_to :User
+end
+
+
+
+  
 
 end 
